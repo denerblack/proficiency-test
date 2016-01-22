140fa8d29a528b05077a88e9e60160ee32335457
Dener W P do Carmo <dener.wilian@gmail.com>
2016-01-22 20:35:57 -0300

    Criado classrooms

---
+++ spec/support/factories/classrooms.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :classroom do
+    student_id nil
+    course_id nil
+    entry_at "2016-01-22 20:10:11"
+  end
+end
