a13a17c7fe952ba7766327325833c5106574abdb
Dener W P do Carmo <dener.wilian@gmail.com>
2016-01-22 19:56:41 -0300

    Corrigido tipo e tamanho da coluna

--- db/migrate/20160122224627_create_students.rb
+++ db/migrate/20160122224627_create_students.rb
@@ -2,7 +2,7 @@ class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
       t.string :name
-      t.string :register_number
+      t.string :register_number, limit: 45
       t.integer :status
 
       t.timestamps
